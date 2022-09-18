7d055e0a5eb3c4be3ee77784f1c4c84b067eaaaa
celine soeiro <celinesoeiro@gmail.com>
2022-09-18 14:17:07 -0300

    ramdom facts card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,27 +1,31 @@
-import axios from 'axios';
-import { useEffect } from 'react';
+interface CardProps {
+  content: string;
+  categories: string[];
+}
 
-import { getRandomFact } from 'services/chuckNorris';
-
-export const Card = () => {
-  useEffect(() => {
-    const res = getRandomFact();
-    console.log(res);
-  });
-
+export const Card: React.FC<CardProps> = ({ content, categories }) => {
   return (
-    <div className="bg-light border-none max-w-sm max-h-64 rounded-lg shadow-lg px-5 py-5">
-      <div className="flex flex-col gap-4 mb-5">
-        <p className="text-xl text-left text-dark">Title</p>
-        <p className="text-lg text-left text-dark">Content</p>
+    <div
+      className="
+      bg-light 
+      rounded-lg border-none 
+      max-w-md max-h-64 
+      overflow-auto
+      shadow-lg 
+      px-6 sm:px-8 py-5
+      w-full"
+    >
+      <div className="flex flex-col gap-4">
+        <p className="text-xl sm:text-2xl text-left sm:text-justify text-dark font-poppins font-semibold">
+          {content}
+        </p>
       </div>
       <div className="flex flex-row gap-3">
-        <span className="inline-block bg-neutral rounded-full px-4 text-sm font-lora font-semibold text-dark">
-          Label #1
-        </span>
-        <span className="inline-block bg-neutral rounded-full px-4 text-sm font-lora font-semibold text-dark">
-          Label #2
-        </span>
+        {categories.map((category) => (
+          <span className="inline-block bg-neutral rounded-full px-4 text-sm font-poppins font-medium text-dark mt-5">
+            {category}
+          </span>
+        ))}
       </div>
     </div>
   );
--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,14 +1,58 @@
+import { useEffect, useState } from 'react';
+import { getRandomFact } from 'services/chuckNorris';
+
 import { Card } from 'components/Card';
 
 export const Hero = () => {
+  const [content, setContent] = useState('');
+  const [categories, setCategories] = useState([]);
+
+  const handleRandomFact = () => {
+    const res = getRandomFact();
+    res.then((data) => {
+      setContent(data.value);
+      setCategories(data.categories);
+    });
+  };
+
+  useEffect(() => {
+    handleRandomFact();
+  }, []);
+
   return (
-    <div className="bg-image bg-bottom bg-no-repeat bg-cover flex w-full h-full">
-      <div className="grid grid-cols-2 w-full">
-        <div className="text-dark w-full">
-          <h1>Chuck Norris Facts</h1>
+    <div className="bg-image bg-bottom bg-no-repeat bg-cover flex w-full h-full py-10 px-20 sm:p-20 items-center content-center justify-center">
+      <div
+        className="
+        grid
+        sm:grid-cols-1 sm:grid-rows-2 
+        md:grid-cols-2 md:grid-rows-1
+        lg:grid-cols-2 lg:grid-rows-1 
+        xl:grid-cols-2 xl:grid-rows-1 
+        w-full max-w-7xl 
+        gap-10 
+        content-center"
+      >
+        <div
+          className="
+          text-dark 
+          w-full h-full 
+          flex justify-center items-center"
+        >
+          <h1
+            className="
+            lg:text-9xl
+            md:text-8xl
+            sm:text-7xl
+            text-5xl
+            font-poppins uppercase font-black italic
+            text-center md:text-left
+            self-start"
+          >
+            Chuck Norris Facts
+          </h1>
         </div>
-        <div className="w-full">
-          <Card />
+        <div className="w-full h-full justify-center items-center flex">
+          <Card categories={categories} content={content} />
         </div>
       </div>
     </div>
